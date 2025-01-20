3e09b5ce0532ec74c71abb2e2beb334cf8545917
AfonsoCastro1983 <afonso.castro@gmail.com>
2025-01-19 20:54:21 -0400

    Alterações cognito e S3 sem lambda

--- cognito/cognito.tf
+++ cognito/cognito.tf
@@ -42,16 +42,6 @@ resource "aws_iam_role_policy_attachment" "lambda_exec_role_policy" {
   policy_arn = aws_iam_policy.lambda_exec_policy.arn
 }
 
-resource "aws_lambda_function" "post_auth_lambda" {
-  filename         = "lambda/post_auth.zip"
-  function_name    = "post-auth-lambda"
-  runtime          = "python3.9"
-  handler          = "lambda_function.lambda_handler"
-  role             = aws_iam_role.lambda_exec_role.arn
-
-  source_code_hash = filebase64sha256("lambda/post_auth.zip")
-}
-
 resource "aws_cognito_user_pool" "user_pool" {
   name = "videoframe-pool"
 
